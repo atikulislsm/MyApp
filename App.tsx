755d404f9b9aa3a41c85a22561e32b0f8215921e
atikulislsm <atikul.islam.atik1971@gmail.com>
2025-06-25 17:28:55 +0600

    First React-Native App

--- App.tsx
+++ App.tsx
@@ -4,24 +4,54 @@
  *
  * @format
  */
-
+import React from 'react';
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text , Button} from 'react-native';
+import{Provider} from 'react-redux';
+import{store} from './src/redux/store';
+import{useAppDispatch, useAppSelector} from './src/redux/hooks';
+import { decrement, increment, reset } from './src/redux/slices/counterSlice';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+const HomeScreen =() =>{
+  const dispatch = useAppDispatch();
+  const value =useAppSelector((state)=>state.counter.value);
+  const isDarkMode =useColorScheme() =='dark';
+  return(
+    <View style={[styles.container, {backgroundColor: isDarkMode ? '#00' : '#fff'}]}>
+      <StatusBar barStyle={isDarkMode ? 'light-content': 'dark-content'}/>
+      <Text style={[styles.text, {color: isDarkMode ? '#fff': '#000'}]}>
+        Counter value: {value}
+      </Text>
+      <View style={styles.buttonGroup}>
+        <Button title="Increment" onPress={() => dispatch(increment())} />
+        <Button title="Decrement" onPress={() => dispatch(decrement())} />
+        <Button title="Reset" onPress={() => dispatch(reset())} />
+      </View>
     </View>
+
   );
-}
+};
+const App = () => {
+  return (
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',  // center vertically
+    alignItems: 'center',      // center horizontally
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    color: '#000',
+  },
+  buttonGroup: {
+    gap: 12, // works in React Native 0.73+; otherwise use margin
   },
 });
 
